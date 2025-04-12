4a496346b74671004ecf6c596e7386ab4ddab2b8
Derin DeNio <derindenio@gmail.com>
2025-04-12 17:24:39 -0600

    Adding data to app component, and header

---
+++ src/components/Course.jsx
@@ -0,0 +1,11 @@
+const Course = ({course}) => {
+    console.log("props are", course)
+    // return (
+    //     <div>  
+    //         <h1><Header text={text}/></h1>
+
+    //     </div>
+    // )
+  }
+
+export default Course
